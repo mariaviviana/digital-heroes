314513eccf80cdfde08d4729682bf62dd857ea88
mariaviviana <elmaiten2@gmail.com>
2020-04-19 13:09:09 -0300

    nada

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ app.listen(3030, () => console.log('Server running in 3030 port'));
 const heroes = JSON.parse(fs.readFileSync('./data/heroes.json', 'utf-8'));
 
 app.use('/heroes', rutaHeroes);
-
 app.use('/', rutaMain);
-
+app.use('/creditos', rutaMain);
 app.use('*', rutaMain);
